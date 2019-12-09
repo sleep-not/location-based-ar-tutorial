d80ac31049142124a4d7d74d91ae4d099b3f95bf
Orestis Sampson <orestis.sampson@iccs.gr>
2019-12-09 11:11:26 +0200

    add multiple places

---
+++ script.js
@@ -0,0 +1,45 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Magnemite1',
+            location: {
+                lat: 37.977938,
+                lng: 23.782951,
+            }
+        },
+        {
+            name: 'Magnemite2',
+            location: {
+                lat: 37.977933,
+                lng: 23.783165,
+            }
+        }
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.5 0.5 0.5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
